39342e0a3feae4428669ec99c849babcbe057701
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-11-17 16:30:23 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,31 @@
+import styled from '@emotion/styled'
+import Box from '@mui/material/Box'
 import React from 'react'
 
+const StyledLink = styled.a(() => ({
+  color: 'gray',
+  textDecoration: 'none',
+  marginRight: 8
+}))
+
 export const Footer = () => {
-  return <div>Footer</div>
+  let targetUrl = 'https://www.google.com'
+  return (
+    <React.Fragment>
+      <Box component='div' m={1} sx={{ textAlign: 'center', color: 'gray' }}>
+        <StyledLink href={targetUrl} target='_blank' rel='noreferrer'>
+          Privacy ·
+        </StyledLink>
+        <StyledLink href={targetUrl} target='_blank' rel='noreferrer'>
+          Terms ·
+        </StyledLink>
+        <StyledLink href={targetUrl} target='_blank' rel='noreferrer'>
+          Cookies ·
+        </StyledLink>
+        <StyledLink href={targetUrl} target='_blank' rel='noreferrer'>
+          More
+        </StyledLink>
+      </Box>
+    </React.Fragment>
+  )
 }
